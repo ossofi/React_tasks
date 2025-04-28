3c58923a90df71e382396e9105b13544a9a0ef1e
ossofi <72688992+ossofi@users.noreply.github.com>
2025-04-28 23:23:45 +0300

    fix Button.jsx

--- my-react-app/src/components/Button/Button.jsx
+++ my-react-app/src/components/Button/Button.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Button.scss';
 
-const Button = ({ onClick, children, className = '', type = 'button' }) => {
-  return (
-    <button type={type} className={`button ${className}`} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+class Button extends Component {
+  render() {
+    const { onClick, children, className = '', type = 'button' } = this.props;
+
+    return (
+      <button type={type} className={`button ${className}`} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+}
 
 export default Button;

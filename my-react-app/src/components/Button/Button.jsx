8d33fa16023c6dca8468b8d7d9b0d0540d566109
sofi <osinskaasona@gmail.com>
2025-04-29 13:20:56 +0300

    task4: add

--- my-react-app/src/components/Button/Button.jsx
+++ my-react-app/src/components/Button/Button.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './Button.scss';
 
-class Button extends Component {
-  render() {
-    const { onClick, children, className = '', type = 'button' } = this.props;
-
-    return (
-      <button type={type} className={`button ${className}`} onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ({ onClick, children, className = '', type = 'button' }) => {
+  return (
+    <button type={type} className={`button ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}; 
 
 export default Button;

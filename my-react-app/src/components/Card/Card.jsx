1e6780515158003102768844e798014c1e295cea
sofi <osinskaasona@gmail.com>
2025-05-15 00:14:36 +0300

    task5: add, fix

--- my-react-app/src/components/Card/Card.jsx
+++ my-react-app/src/components/Card/Card.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button.jsx';
 import './Card.scss';
 
 const MenuCard = ({ product, onAddToCart }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (e) => {
+    setQuantity(Math.max(1, parseInt(e.target.value) || 1));
+  };
+
   const handleAddToCart = () => {
-    onAddToCart(product);
+    onAddToCart(quantity); 
   };
 
   return (
-    <div className="item-card" key={product.id}>
+    <div className="item-card">
       <div className="item-card-left">
         <img src={product.img} alt={product.meal} className="item-img" />
       </div>
@@ -22,7 +28,12 @@ const MenuCard = ({ product, onAddToCart }) => {
         </p>
         <div className="item-footer">
           <div className="item-amount">
-            <span>1</span>
+            <input
+              type="number"
+              min="1"
+              value={quantity}
+              onChange={handleQuantityChange}
+            />
           </div>
           <Button className="add-to-cart" onClick={handleAddToCart}>
             Add to Cart
